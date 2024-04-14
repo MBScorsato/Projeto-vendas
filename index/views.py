563c9ed61388bd8a1bb047d249cd2ab9364ecb9b
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-04-13 23:34:55 -0300

    backend criado com a possibilidade de ir no admin preecher o nome do alto do site, onde esta o navbar

--- index/views.py
+++ index/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from index.models import bootstrap_navbar
 
-#grgbttyny
+
 def index(request):
     if request.method == 'GET':
-        return render(request, 'index.html')
+        ultimo_objeto_salvo = bootstrap_navbar.objects.all().order_by('-id').first()
+        return render(request, 'index.html',{
+            'ultimo_objeto_salvo': ultimo_objeto_salvo,
+        })
     elif request.method == 'POST':
         return render(request, 'index.html')
