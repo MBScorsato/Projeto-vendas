707f1ea6bf0f48dbad81d5d0e2873bdfbb269c84
Murilo B. Scorsato <scorsatomurilo@gmail.com>
2024-04-13 23:22:23 -0300

    bootstrap_navbar banco criado

--- index/admin.py
+++ index/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from index.models import bootstrap_navbar
 
-# Register your models here.
+admin.site.register(bootstrap_navbar)
--- index/models.py
+++ index/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class bootstrap_navbar(models.Model):
+    nome_navbar = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_navbar
+
+    class Meta:
+        verbose_name = "Nome site"
+
+    class Meta:
+        verbose_name_plural = "Nome site"
